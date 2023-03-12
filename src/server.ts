635d516cb54f5ee10724792e5dbfcf9e4c92e604
Serhat AK <srhtak777@gmail.com>
2023-03-12 17:46:10 +0300

    jwt

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import router from './router';
-
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.status(200);
